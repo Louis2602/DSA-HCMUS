5091102760950c42e0593a4dd6a5c13aa25caea6
Louis2602 <trantunglam135790@gmail.com>
2022-05-06 14:28:43 +0700

    update merge sort

--- Sorting/mergesort.cpp
+++ Sorting/mergesort.cpp
@@ -1,8 +1,74 @@
 #include <iostream>
 using namespace std;
+void merge(int a[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    int *L = new int[n1];
+    int *R = new int[n2];
 
+    for (i = 0; i < n1; i++)
+        L[i] = a[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = a[m + 1 + j];
+
+    // Merge 2 temp array into the original array
+    i = j = 0;
+    k = l;
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            a[k] = L[i];
+            i++;
+        }
+        else
+        {
+            a[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < n1)
+    {
+        a[k] = L[i];
+        i++;
+        k++;
+    }
+    while (j < n2)
+    {
+        a[k] = R[j];
+        j++;
+        k++;
+    }
+    delete[] L;
+    delete[] R;
+}
+void mergeSort(int a[], int l, int r)
+{
+    if (l < r)
+    {
+        int m = (l + r) / 2;
+        mergeSort(a, l, m);
+        mergeSort(a, m + 1, r);
+        merge(a, l, m, r);
+    }
+}
+void print(int a[], int n)
+{
+    for (int i = 0; i < n; i++)
+        cout << a[i] << " ";
+    cout << '\n';
+}
 int main()
 {
-    cout << "Hello world";
+    int a[] = {12, 11, 13, 5, 6, 3};
+    int n = sizeof(a) / sizeof(a[0]);
+    cout << "Before: ";
+    print(a, n);
+    cout << "After sorting: ";
+    mergeSort(a, 0, n - 1);
+    print(a, n);
     return 0;
 }
