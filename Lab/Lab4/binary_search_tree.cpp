1062a8c96438c45f56d2c94254ad6f8a388451d5
Louis2602 <trantunglam135790@gmail.com>
2022-05-26 22:25:22 +0700

    update bst

--- Lab/Lab4/binary_search_tree.cpp
+++ Lab/Lab4/binary_search_tree.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <queue>
 using namespace std;
 
 struct NODE {
@@ -10,8 +11,7 @@ struct NODE {
 NODE *createNode(int data) {
     NODE *p = new NODE;
     p->data = data;
-    p->left = NULL;
-    p->right = NULL;
+    p->left = p->right = NULL;
     return p;
 }
 
@@ -37,15 +37,82 @@ void printPreorder(NODE* pRoot)
     /* now recur on right subtree */
     printPreorder(pRoot->right);
 }
+void LevelOrder(NODE* pRoot) {
+    if (pRoot == NULL)
+        return;
+    queue<NODE *> Q;
+    Q.push(pRoot);
+    while(!Q.empty()) {
+        NODE *cur = Q.front();
+        cout << cur->data << " ";
+        if(cur->left != NULL)
+            Q.push(cur->left);
+        if(cur->right != NULL)
+            Q.push(cur->right);
+        Q.pop();
+    }
+    cout << '\n';
+}
+
+int countNode(NODE* pRoot) {
+    if(pRoot == NULL)
+        return 0;
+    return 1 + countNode(pRoot->left) + countNode(pRoot->right);
+}
+int sumNode(NODE* pRoot) {
+    if(pRoot == NULL)
+        return 0;
+    return (pRoot->data + sumNode(pRoot->left) + sumNode(pRoot->right));
+}
+
+NODE* Search(NODE* pRoot, int x) {
+    if(pRoot == NULL)
+        return pRoot;
+    if(x > pRoot->data)
+        Search(pRoot->right, x);
+    else if(x < pRoot->data)
+        Search(pRoot->left, x);
+    else
+        return pRoot;
+}
+
+NODE *createTree(int a[], int n, int i) {
+    NODE *pRoot = NULL;
+    if(i < n) {
+        pRoot = createNode(a[i]);
+        pRoot->left = createTree(a, n, 2*i+1);
+        pRoot->right = createTree(a, n, 2*i+2);
+    }
+    return pRoot;
+}
+
+void inOrder(NODE *pRoot) {
+    if(pRoot != NULL) {
+        inOrder(pRoot->left);
+        cout << pRoot->data << " ";
+        inOrder(pRoot->right);
+    }
+}
 int main() {
     NODE *pRoot = NULL;
-    Insert(pRoot, 5);
-    Insert(pRoot, 7);
-    Insert(pRoot, 9);
+    int a[] = {1, 2, 3, 4, 5, 6};
     Insert(pRoot, 1);
-    Insert(pRoot, 10);
-    Insert(pRoot, 11);
-    Insert(pRoot, 15);
-    printPreorder(pRoot);
+    Insert(pRoot, 2);
+    Insert(pRoot, 3);
+    Insert(pRoot, 4);
+    Insert(pRoot, 5);
+    Insert(pRoot, 6);
+    //printPreorder(pRoot);
+    LevelOrder(pRoot);
+    /*cout << "\nNumber of nodes: " << countNode(pRoot) << '\n';
+    cout << "Sum of all nodes: " << sumNode(pRoot) << '\n';
+    NODE *search = Search(pRoot, 6);
+    if(search != NULL)
+        cout << "Found!";
+    else
+        cout << "Not found!";*/
+    int n = sizeof(a) / sizeof(a[0]);
+    NODE *r = createTree(a, n, 0);
+    LevelOrder(r);
     return 0;
 }
