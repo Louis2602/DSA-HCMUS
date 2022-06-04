5321270db6e0cad0657e3d9c8fb98cfdadf6b14c
Louis2602 <trantunglam135790@gmail.com>
2022-06-04 22:03:20 +0700

    update

--- Lab/Lab1/filehandling.cpp
+++ Lab/Lab1/filehandling.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 #include <vector>
 
@@ -8,88 +9,57 @@ using namespace std;
 struct Examinee
 {
     string id;
-    float math, literature, physic, chemistry, biology, history, geography, civic_education, natural_science,
-        social_science, foreign_language;
+    float grade[11];
 };
 
-void readFile(char *filename, Examinee e[], int n)
+Examinee readExaminee(string line_info)
 {
-    fstream fs(filename, ios::in);
-    int i = 0;
-    string s;
-    getline(fs, s);
-    char c;
-    for (int i = 0; i < n; i++)
+    stringstream ss(line_info);
+    string tmp;
+    Examinee e;
+    getline(ss, e.id, ','); // read id
+    getline(ss, tmp, ',');  // read name
+    for (int i = 0; i < 11; i++)
     {
-        getline(fs, e[i].id, ',');
-        fs.ignore();
-        getline(fs, s, ',');
-        e[i].math = atof(s.c_str());
-        getline(fs, s, ',');
-        e[i].literature = atof(s.c_str());
-        getline(fs, s, ',');
-        e[i].physic = atof(s.c_str());
-        getline(fs, s, ',');
-        e[i].chemistry = atof(s.c_str());
-        getline(fs, s, ',');
-        e[i].biology = atof(s.c_str());
-        getline(fs, s, ',');
-        e[i].history = atof(s.c_str());
-        getline(fs, s, ',');
-        e[i].geography = atof(s.c_str());
-        getline(fs, s, ',');
-        e[i].civic_education = atof(s.c_str());
-        getline(fs, s, ',');
-        e[i].natural_science = atof(s.c_str());
-        getline(fs, s, ',');
-        e[i].social_science = atof(s.c_str());
-        getline(fs, s, ',');
-        e[i].foreign_language = atof(s.c_str());
-        getline(fs, s, ',');
-        getline(fs, s);
+        getline(ss, tmp, ',');
+        if (tmp == "")
+            e.grade[i] = 0;
+        else
+            e.grade[i] = stof(tmp);
     }
-    fs.close();
+    return e;
 }
-
-int readLines(char *filename)
+vector<Examinee> readExamineeList(string fname)
 {
-    fstream fs(filename, ios::in);
-    string s;
-    int n = 0;
+    fstream fs(fname, ios::in);
+    vector<Examinee> ve;
+    Examinee t;
+    string tmp;
+    getline(fs, tmp); // skip intro line
     while (!fs.eof())
     {
-        getline(fs, s);
-        n++;
-    }
-    return n;
-}
-Examinee readExaminee(string line_info, Examinee *e)
-{
-    int k = stoi(line_info);
-    return e[k];
-}
-void writeTotal(vector<Examinee> examinee_list, char *out_file_name, int n, Examinee *e)
-{
-    fstream fs(out_file_name, ios::out);
-    int s = 0;
-    float total_score;
-    for (int i = 0; i < n - 1; i++)
-    {
-        total_score = e[i].math + e[i].literature + e[i].physic + e[i].chemistry + e[i].biology + e[i].history + e[i].geography + e[i].civic_education + e[i].social_science + e[i].foreign_language;
-        fs << e[i].id << " " << total_score << endl;
+        getline(fs, tmp);
+        t = readExaminee(tmp);
+        ve.push_back(t);
     }
     fs.close();
+    return ve;
 }
 int main()
 {
-    char filename[] = "data.txt";
-    char out_file_name[] = "output.txt";
-    int n = readLines(filename);
-    Examinee *e = new Examinee[n];
-    readFile(filename, e, n);
-    // Examinee t;
-    // t = readExaminee("3", n, e);
-    vector<Examinee> examinee_list;
-    writeTotal(examinee_list, out_file_name, n, e);
+    Examinee e = readExaminee("BD1200001,,4.0,5.0,,1,,4.25,7.0,7.75,,,2.0,N1,BinhDinh");
+    cout << e.id << '\n';
+    for (int i = 0; i < 11; i++)
+        cout << e.grade[i] << '\t';
+    // vector<Examinee> ve = readExamineeList("data.txt");
+    // for (int i = 0; i < ve.size(); i++)
+    // {
+    //     for (int j = 0; j < 11; j++)
+    //     {
+    //         cout << ve[i].grade[j] << '\t';
+    //     }
+    //     cout << '\n';
+    // }
+
     return 0;
 }
