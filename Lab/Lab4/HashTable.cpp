28bfd93bd72e10d7bedc77d8ccfa978ed68a1284
Louis2602 <trantunglam135790@gmail.com>
2022-07-03 09:25:46 +0700

    update hash table

--- Lab/Lab4/HashTable.cpp
+++ Lab/Lab4/HashTable.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <fstream>
-#include <sstream>
 #include <vector>
 #include <string>
 #include <math.h>
@@ -15,80 +14,59 @@ struct Company
 
 vector<Company> readCompanyList(string file_name)
 {
-	ifstream fin(file_name);
-	string s, name, tax, address;
-	getline(fin, s); // Bỏ qua dòng đầu tiên
-	Company C;
-	vector<Company> vC;
-	while (!fin.eof())
+	fstream fs(file_name, ios::in);
+	string s;
+	getline(fs, s);
+	Company c;
+	vector<Company> cList;
+	while (!fs.eof())
 	{
-		getline(fin, s);
-		stringstream ss(s);
-		getline(ss, name, '|');
-		getline(ss, tax, '|');
-		getline(ss, address);
-		C.name = name;
-		C.profit_tax = tax;
-		C.address = address;
-		vC.push_back(C);
+		getline(fs, c.name, '|');
+		getline(fs, c.profit_tax, '|');
+		getline(fs, c.address);
+		cList.push_back(c);
 	}
-	return vC;
+	fs.close();
+	return cList;
 }
 
 long long hashCalculate(int p, int i, int m)
 {
-	long long result = 1;
+	long long res = 1;
 	for (int j = 0; j < i; j++)
 	{
-		result *= p;
-		result = result % m;
+		res *= p;
+		res %= m;
 	}
-	return result;
+	return res;
 }
 
 long long hashString(string company_name)
 {
 	int len = company_name.size() > 20 ? 20 : company_name.size();
 	int begin = company_name.size() > 20 ? company_name.size() - 20 : 0;
-	// Nếu chuỗi nhiều hơn 20 thì độ dài chuỗi là 20 và kí tự bắt đầu là len - 20
-	// Nếu chuỗi ít hơn hoặc bằng 20 thì độ dài chuỗi là len và kí tự bắt đầu là 0
-	/*
-	if(company_name.size() > 20)
-		length = 20;
-	else
-		length = company_name.size();
-	*/
 	long long m = pow(10, 9) + 9;
 	long long hashValue = 0;
 	for (int i = begin; i < (begin + len); i++)
-	{
 		hashValue += int(company_name[i]) * hashCalculate(31, i - begin, m);
-		hashValue = hashValue % m;
-	}
-	return hashValue;
+	return hashValue % m;
 }
 
 Company *createHashTable(vector<Company> list_company)
 {
 	Company *pC = new Company[2000];
 	for (int i = 0; i < 2000; i++)
-	{
 		pC[i] = {"", "", ""};
-	}
 	for (int i = 0; i < list_company.size(); i++)
 	{
 		int hval = hashString(list_company[i].name);
 		if (pC[hval % 2000].name == "")
-		{
 			pC[hval % 2000] = {list_company[i].name, list_company[i].profit_tax, list_company[i].address};
-		}
 		else
 		{
 			int temp = hashString(list_company[i].name) % 2000 + 1;
 			while (pC[temp].name != "")
-			{
 				temp = (temp + 1) % 2000;
-			}
 			pC[temp] = {list_company[i].name, list_company[i].profit_tax, list_company[i].address};
 		}
 	}
@@ -99,16 +77,12 @@ void Insert(Company *hash_table, Company company)
 {
 	int hval = hashString(company.name);
 	if (hash_table[hval % 2000].name == "")
-	{
 		hash_table[hval % 2000] = {company.name, company.profit_tax, company.address};
-	}
 	else
 	{
 		int temp = hashString(company.name) % 2000 + 1;
 		while (hash_table[temp].name != "")
-		{
 			temp = (temp + 1) % 2000;
-		}
 		hash_table[temp] = {company.name, company.profit_tax, company.address};
 	}
 }
