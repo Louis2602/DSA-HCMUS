4c19c591f15ad152f457e92fdbb7cb4488ffe6c7
Louis2602 <trantunglam135790@gmail.com>
2022-07-11 17:02:24 +0700

    update

--- Lab/Lab4/checkSubtreeAndIdentical.cpp
+++ Lab/Lab4/checkSubtreeAndIdentical.cpp
@@ -1,58 +1,73 @@
 #include <iostream>
 using namespace std;
 
-struct Node {
+struct Node
+{
 	int key;
 	Node *left;
 	Node *right;
 };
 
-Node *createNode(int x) {
+Node *createNode(int x)
+{
 	Node *p = new Node;
 	p->key = x;
 	p->left = p->right = NULL;
 	return p;
 }
 
-void Insert(Node *&root, int x) {
-	if(root == NULL)
+void Insert(Node *&root, int x)
+{
+	if (root == NULL)
 	{
 		root = createNode(x);
 		return;
 	}
-	if(x > root->key) {
-		Insert(root->right);
+	if (x > root->key)
+	{
+		Insert(root->right, x);
 	}
-	else if(x < root->key) {
-		Insert(root->left);
+	else if (x < root->key)
+	{
+		Insert(root->left, x);
 	}
-	else return;
+	else
+		return;
 }
-bool areIdentical(node * root1, node *root2)
+bool areIdentical(Node *root1, Node *root2)
 {
-    if (root1 == NULL && root2 == NULL)
-        return true;
- 
-    if (root1 == NULL || root2 == NULL)
-        return false;
-    return (root1->data == root2->data &&
-            areIdentical(root1->left, root2->left) &&
-            areIdentical(root1->right, root2->right));
+	if (root1 == NULL && root2 == NULL)
+		return true;
+
+	if (root1 == NULL || root2 == NULL)
+		return false;
+	return (root1->key == root2->key &&
+			areIdentical(root1->left, root2->left) &&
+			areIdentical(root1->right, root2->right));
 }
- 
-bool isSubtree(node *T, node *S)
+bool isMirror(Node *root1, Node *root2)
 {
-    if (S == NULL)
-        return true;
-    if (T == NULL)
-        return false;
-    if (areIdentical(T, S))
-        return true;
-    return isSubtree(T->left, S) ||
-        isSubtree(T->right, S);
+	if (root1 == NULL && root2 == NULL)
+		return true;
+	if (root1 == NULL || root2 == NULL)
+		return false;
+	return (root1->key == root2->key &&
+			areIdentical(root1->left, root2->right) &&
+			areIdentical(root1->right, root2->left));
+}
+bool isSubtree(Node *T, Node *S)
+{
+	if (S == NULL)
+		return true;
+	if (T == NULL)
+		return false;
+	if (areIdentical(T, S))
+		return true;
+	return isSubtree(T->left, S) ||
+		   isSubtree(T->right, S);
 }
 
-
-int main() {
+int main()
+{
 	return 0;
 }
