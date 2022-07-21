3d0fce6e47c107354e71e7da03ee1816dbbb8d86
Louis2602 <trantunglam135790@gmail.com>
2022-07-21 20:16:01 +0700

    update hashtable

--- Lab/Lab4/HashTable.cpp
+++ Lab/Lab4/HashTable.cpp
@@ -52,71 +52,56 @@ long long hashString(string company_name)
 	return hashValue % m;
 }
 
-Company *createHashTable(vector<Company> list_company)
-{
+Company *createHashTable(vector<Company> list_company) {
 	Company *pC = new Company[2000];
-	for (int i = 0; i < 2000; i++)
-		pC[i] = {"", "", ""};
-	for (int i = 0; i < list_company.size(); i++)
-	{
+	for(int i=0; i<list_company.size(); i++) {
 		int hval = hashString(list_company[i].name);
-		if (pC[hval % 2000].name == "")
-			pC[hval % 2000] = {list_company[i].name, list_company[i].profit_tax, list_company[i].address};
-		else
-		{
-			int temp = hval % 2000 + 1;
-			while (pC[temp].name != "")
-				temp = (temp + 1) % 2000;
-			pC[temp] = {list_company[i].name, list_company[i].profit_tax, list_company[i].address};
+
+		if(pC[hval % 2000].name == "") 
+			pC[hval % 2000] = list_company[i];	
+		else {
+			int tmp = hval % 2000 + 1;
+			while(pC[tmp].name != "") {
+				tmp = (tmp + 1) % 2000;
+			}
+			pC[tmp] = list_company[i];
 		}
-	}
+	}	
 	return pC;
 }
 
-void Insert(Company *hash_table, Company company)
-{
+void Insert(Company *hash_table, Company company) {
 	int hval = hashString(company.name);
-	if (hash_table[hval % 2000].name == "")
-		hash_table[hval % 2000] = {company.name, company.profit_tax, company.address};
-	else
-	{
-		int temp = hval % 2000 + 1;
-		while (hash_table[temp].name != "")
-			temp = (temp + 1) % 2000;
-		hash_table[temp] = {company.name, company.profit_tax, company.address};
+	if(hash_table[hval % 2000].name == "")
+		hash_table[hval % 2000] = company;
+	else {
+		int tmp = hval % 2000 + 1;
+		while(hash_table[tmp].name != "")
+			tmp = (tmp + 1) % 2000;
+		hash_table[tmp] = company;
 	}
 }
 
-Company *Search(Company *hash_table, string company_name)
-{
-	int hval = hashString(company_name) % 2000;
-	while (hash_table[hval].name != company_name)
-	{
-		if (hash_table[hval].name == "")
+Company *Search(Company *hash_table, string company_name) {
+	int hval = hashString(company_name);
+	while(hash_table[hval % 2000].name != company_name) {
+		if(hash_table[hval % 2000].name == "")
 			return NULL;
 		hval = (hval + 1) % 2000;
 	}
-	return &hash_table[hval];
+	return &hash_table[hval % 2000];
 }
-int main()
-{
+
+int main() {
 	Company *pC = createHashTable(readCompanyList("MST.txt"));
 
 	for (int i = 0; i < 2000; i++)
-	{
 		if (pC[i].name != "")
-		{
 			cout << i << "  " << pC[i].name << endl;
-		}
-	}
-	// Company myC{"ABC", "123456", "Khongco"};
-	// Insert(pC, myC);
-	// cout << Search(pC, "ABC")->profit_tax;
-	/*
-	vector<Company> vC = readCompanyList("MST.txt");
-	for(int i = 0; i < vC.size(); i++){
-		cout << vC[i].name << endl;
-	}
-	*/
+
+	Company myC{"ABC", "123456", "Khongco"};
+	Insert(pC, myC);
+	Company *found = Search(pC, "ABC");
+	cout << found->name << " " << found->profit_tax << " " << found->address;
 	return 0;
 }
